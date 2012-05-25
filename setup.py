9c8de90a507cefc6c78022b488aab35a1b727094
bcoe <=>
2012-05-25 14:39:21 -0700

    Rolling version #.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="groundhogday",
-      version="0.0.1",
+      version="0.0.2",
       description="GroundhogDay lets you repeat an operation until you get it right.",
       author="Benjamin Coe",
       author_email="ben@attachments.me",
