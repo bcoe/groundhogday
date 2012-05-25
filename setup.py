e15e0551aa877f8853638c4e1f89c5f56883d065
bcoe <=>
2012-05-25 14:56:33 -0700

    Updating package information.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 
 setup(name="groundhogday",
-      version="0.0.2",
+      version="0.0.3",
       description="GroundhogDay lets you repeat an operation until you get it right.",
       author="Benjamin Coe",
       author_email="ben@attachments.me",
       entry_points = {},
-      url="https://github.com/attachmentsme/groundhogday",
+      url="https://github.com/bcoe/groundhogday",
       packages = find_packages(),
       install_requires = ['Pytoad==1.0.7'],
       tests_require=['nose']
